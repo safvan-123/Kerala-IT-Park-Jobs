7ae3cd46c3b493f739c0bbc30a7cef90a68931f1
sahilgitt <sahilshabeer117@gmail.com>
2026-06-01 17:09:28 +0530

    Use Firebase env variables

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -1,10 +1,25 @@
-import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
+// import admin from "firebase-admin";
+// import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// export default admin;
+
+
+import admin from "firebase-admin";
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 export default admin;
